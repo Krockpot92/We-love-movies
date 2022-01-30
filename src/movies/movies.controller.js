cedd0d3398b2a1505d3d1a268a81c08007215747
Steven Lisboa <elstevo92@gmail.com>
2022-01-30 15:57:43 -0500

    sd

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -7,6 +7,25 @@ function list(req, res, next) {
     .catch(next);
 }
 
+function movieExists(req, res, next) {
+  moviesService
+    .read(req.params.movieId)
+    .then((movie) => {
+      if (movie) {
+        res.locals.movie = movie;
+        return next();
+      }
+      next({ status: 404, message: `movie cannot be found.` });
+    })
+    .catch(next);
+}
+
+function read(req, res) {
+  const { movie: data } = res.locals;
+  res.json({ data });
+}
+
 module.exports = {
   list,
+  read: [movieExists, read],
 };
--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
-
 const cors = require("cors");
 
-router.use(cors())
+// router.route("/:movieId([0-9]+)")
+//   .get(controller.read)
+//   .all(methodNotAllowed);
 
 router
   .route("/")
