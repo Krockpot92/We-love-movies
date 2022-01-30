c12bcfe1fd14e624ea1071b5d13dc263ef7ef0db
Steven Lisboa <elstevo92@gmail.com>
2022-01-30 15:58:44 -0500

    sdasd

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -3,9 +3,9 @@ const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 const cors = require("cors");
 
-// router.route("/:movieId([0-9]+)")
-//   .get(controller.read)
-//   .all(methodNotAllowed);
+router.route("/:movieId([0-9]+)")
+  .get(controller.read)
+  .all(methodNotAllowed);
 
 router
   .route("/")
